86a657ff1accaba4e9d59eb8c16939d500ff5539
Andrew <zezba9000@gmail.com>
2019-06-06 22:00:16 -0700

    readme

--- TestOutput/WASM/build.bat
+++ TestOutput/WASM/build.bat
@@ -1,2 +1,1 @@
-D:\Dev\SDK\emsdk\emscripten\1.38.30\emcc.bat ..\PortableTestApp.c -o cs2x.html -O3
- --js-library cs2x_display.js -w
+D:\Dev\SDK\emsdk\emscripten\1.38.30\emcc.bat ..\PortableTestApp.c -o cs2x.html -O3 --js-library cs2x_display.js -w
