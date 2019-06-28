176f98c18f3d186592a87693ef44be95d3b499f8
Andrew <zezba9000@gmail.com>
2019-06-28 14:58:43 -0700

    simplified extern

--- CS2X.CoreLib/DateTime.cs
+++ CS2X.CoreLib/DateTime.cs
@@ -5,15 +5,10 @@ namespace System
 	[NativeType(NativeTarget.C)]
 	struct time_t
 	{
-		public static implicit operator long(time_t value)
-		{
-			return 0;
-		}
-
-		public static implicit operator time_t(long value)
-		{
-			return new time_t();
-		}
+		#pragma warning disable CS0626
+		public static extern implicit operator long(time_t value);
+		public static extern implicit operator time_t(long value);
+		#pragma warning restore CS0626
 	}
 
 	public struct DateTime
